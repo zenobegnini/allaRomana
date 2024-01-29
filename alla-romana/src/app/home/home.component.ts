6244e35f9d420562b4bb46480d63b6eb1b54efc5
Andrea Baldassari <91703230+andreabalda02@users.noreply.github.com>
2024-01-29 11:17:53 +0100

    home component

--- alla-romana/src/app/home/home.component.ts
+++ alla-romana/src/app/home/home.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  transactions: any[] = [1, 2, 3, 4]
+
 
 }
