d7a0a5cef39aaaedc766d0cb9674700db6f7b956
gab03gab <gabrielnori03@gmail.com>
2024-01-29 11:33:42 +0100

    [EDIT] style

--- alla-romana/src/app/app.component.ts
+++ alla-romana/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
-
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule],
+  imports: [CommonModule, RouterOutlet, RouterModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
